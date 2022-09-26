f65a87e947ed3cbd85f249d33420649141590d45
nazar <nazar131019@gmail.com>
2022-09-26 22:05:29 +0300

    fix

--- database/seeders/ServiceTableSeeder.php
+++ database/seeders/ServiceTableSeeder.php
@@ -57,7 +57,7 @@ class ServiceTableSeeder extends Seeder
             'price' => '800-1000',
             'prev' => 'prew.jpg',
             'bg' => 'bg.jpg',
-            'media' => '5.mp4,6.mp4,3.mp4,4.mp4,7.mp4,8.mp4,9.mp4,10.mp4,11.mp4,12.mp4,13.mp4,14.mp4,15.mp4,16.mp4,17.mp4,20.mp4,21.mp4',
+            'media' => '6.mp4,5.mp4,3.mp4,4.mp4,7.mp4,8.mp4,9.mp4,10.mp4,11.mp4,12.mp4,13.mp4,14.mp4,15.mp4,16.mp4,17.mp4,20.mp4,21.mp4',
         ]);
     }
 }
