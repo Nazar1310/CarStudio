5c0575ed7adb183e7acb5b2d006568d9e8d8f1f7
nazar <nazar131019@gmail.com>
2022-12-06 01:03:20 +0200

    fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,7 +13,10 @@
     <meta property="og:url" content="{{Request::url()}}">
     <meta property="og:title" content="@yield('title')">
     <link rel="canonical" href="{{Request::url()}}"/>
-    <link href="/img/favicon.ico" rel="icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 
